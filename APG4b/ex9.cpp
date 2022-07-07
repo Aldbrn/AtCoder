3d9eff2808656142f6ddda0c70b25a03fd3879ba
Aldbrn <aldbrn.xxx@gmail.com>
2022-07-07 17:47:45 +0900

    add new file

---
+++ APG4b/ex9.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, a, b;
+    cin >> x >> a >> b;
+
+    // 1.の出力
+    x++;
+    cout << x << endl;
+    x *= (a + b);
+
+    // ここにプログラムを追記
+}
